63e88fc25d88549d7f130df8f2c17f3c77b0bbc9
SogolCodesHere <sogolsalekii@gmail.com>
2025-04-18 21:21:49 +0200

    Med Libs

---
+++ Kickstarters/madLibs.py
@@ -0,0 +1,14 @@
+# Project Name: Med Libs Game
+# Date: 18.04.2025
+# Author: Sogol Saleki
+# Description:
+# Features:
+# Requirements:
+
+noun1 = input("Enter a noun:")
+age = input("Enter a number:")
+adj1 = input("Enter an adjective:")
+verb = input("Enter a verb:")
+
+print(f"Once upon a time, there was this {noun1}, who is {age} years old.")
+print(f"Yesterday, this {noun1} learned a bad word which was a really {adj1} word, meaning to {verb}...")
